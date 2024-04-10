41b695129257c3089391e0b89668ab137c5537ed
Robinson-Joseph <josephjrobinson@pm.me>
2024-04-09 21:00:36 -0400

    First commit

---
+++ encoder.py
@@ -0,0 +1,28 @@
+
+
+def main():
+    while True:
+        print("Menu:")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        input = int(input("Please enter an option: "))
+        stored = ""
+        match input:
+            case 1: stored = encoder()
+            case 2: decoder()
+            case 3: exit(200)
+
+if "__name__" == "__main__":
+    main()
+
+
+def encoder():
+    unencoded = input("Please enter your password to encode: ")
+    encoded = ""
+    for i in unencoded:
+        encoded = encoded + str(int(i)+3)
+    print("Your password has been encoded and stored!")
+    return encoded
+
