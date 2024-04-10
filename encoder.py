16c1fad6729a5cad54b45707bde163553d6fd37c
Robinson-Joseph <josephjrobinson@pm.me>
2024-04-09 21:08:12 -0400

    2

--- encoder.py
+++ encoder.py
@@ -1,21 +1,5 @@
 
 
-def main():
-    while True:
-        print("Menu:")
-        print("-------------")
-        print("1. Encode")
-        print("2. Decode")
-        print("3. Quit")
-        input = int(input("Please enter an option: "))
-        stored = ""
-        match input:
-            case 1: stored = encoder()
-            case 2: decoder()
-            case 3: exit(200)
-
-if "__name__" == "__main__":
-    main()
 
 
 def encoder():
@@ -25,4 +9,25 @@ def encoder():
         encoded = encoded + str(int(i)+3)
     print("Your password has been encoded and stored!")
     return encoded
+
+def main():
+    while True:
+        print("Menu:")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        entered = int(input("Please enter an option: "))
+        stored = ""
+        match entered:
+            case 1: stored = encoder()
+            case 2: decoder()
+            case 3: exit(200)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
 
